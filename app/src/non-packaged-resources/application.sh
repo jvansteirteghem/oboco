6b235658aacc1580617f96f4bbba9d5bc04c2624
jeeto jeeto <jeeto.jeeto@gmail.com>
2021-11-14 11:52:09 +0100

    options

--- app/src/non-packaged-resources/application.sh
+++ app/src/non-packaged-resources/application.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-java -cp libs/*:oboco-app-${project.version}.jar com.gitlab.jeeto.oboco.Server
+JAVA_OPTIONS=
 # raspberry pi
-#java -Dos.arch=armv71 -cp libs/*:oboco-app-${project.version}.jar com.gitlab.jeeto.oboco.Server
+#JAVA_OPTIONS="-Dos.arch=armv71"
+
+java ${JAVA_OPTIONS} -cp libs/*:oboco-app-${project.version}.jar com.gitlab.jeeto.oboco.Server
